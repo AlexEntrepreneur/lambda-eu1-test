9517e1ed079b8b5302feba9b6bac7c487f484226
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-03-21 16:11:19 +0000

    Stretch: Install + Use DotEnv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const env = require('dotenv').config();
 const server = require('./server.js');
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`server lisening on port ${PORT}...`);
