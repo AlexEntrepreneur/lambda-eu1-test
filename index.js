25344f361d490255ad29700d1f899e07f562ba70
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-03-21 09:57:09 +0000

    Add Heroku Port Settings

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require('./server.js');
 
-server.listen(4000, () => {
-  console.log('server lisening on port 4000...');
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`server lisening on port ${PORT}...`);
 });
